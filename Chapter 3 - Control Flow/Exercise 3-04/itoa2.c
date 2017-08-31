305abf2f5ea582ea4ece37b4d47757c7b06b1f21
clydeev <clydevelasquez@outlook.com>
2017-08-31 12:26:49 +0800

    Solve Exercise 3-04

---
+++ Chapter 3 - Control Flow/Exercise 3-04/itoa2.c
@@ -0,0 +1,62 @@
+/*
+ * Filename: itoa2.c
+ * Date:  August 31, 2017
+ * Author: Clyde M. Velasquez
+ *
+ * The C Programming Language
+ * by Brian W. Kernighan and Dennis M. Ritchie
+ *
+ * Exercise 3-4. In a two's complement number representation, 
+ * our version of itoa does not handle the larget negative number,
+ * that is, the value of n equal to -(2^wordsize-1). Explain why not.
+ * Modify it to print that value correctly, regardless of the 
+ * machine on which it runs.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#define SIZE 100
+
+void itoa(int n, char s[]);
+void reverse(char s[]);
+
+int main() {
+	int n = INT_MIN;
+	char s[SIZE];
+
+	printf("Integer: %d\n", n);
+	itoa(n, s);
+	printf("String: %s\n", s);
+
+	n = INT_MAX;
+	printf("Integer: %d\n", n);
+	itoa(n, s);
+	printf("String: %s\n", s);
+
+	return 0;
+}
+
+/*
+ * No equivalent positive number when we convert the largest
+ * negative number to positive so it will remain the same. 
+ */
+void itoa(int n, char s[]) {
+	int i = 0, sign = n;
+	
+	do { /* generate digits in reverse order */
+		s[i++] = abs(n % 10) + '0';
+	} while ((n /= 10) != 0); 
+
+	if (sign < 0)
+		s[i++] = '-';
+
+	s[i] = '\0';
+	reverse(s);
+}
+
+void reverse(char s[]) {
+	int c, i, j;
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+		c = s[i], s[i] = s[j], s[j] = c;
+	}
+}

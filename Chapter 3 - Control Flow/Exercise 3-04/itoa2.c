901d06e1c0eaf7a23229187c2dd4aa5d9424e9ae
clydeev <clydevelasquez@outlook.com>
2017-09-06 12:44:59 +0800

    Remove reverse function from the prototypes

--- Chapter 3 - Control Flow/Exercise 3-04/itoa2.c
+++ Chapter 3 - Control Flow/Exercise 3-04/itoa2.c
@@ -18,7 +18,6 @@
 #define SIZE 100
 
 void itoa(int n, char s[]);
-void reverse(char s[]);
 
 int main() {
 	int n = INT_MIN;
@@ -34,6 +33,13 @@ int main() {
 	printf("String: %s\n", s);
 
 	return 0;
+}
+
+void reverse(char s[]) {
+	int c, i, j;
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+		c = s[i], s[i] = s[j], s[j] = c;
+	}
 }
 
 /*
@@ -52,11 +58,4 @@ void itoa(int n, char s[]) {
 
 	s[i] = '\0';
 	reverse(s);
-}
-
-void reverse(char s[]) {
-	int c, i, j;
-	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
-		c = s[i], s[i] = s[j], s[j] = c;
-	}
 }
